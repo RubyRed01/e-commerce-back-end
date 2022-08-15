2aeab5d8e1afcf50f453a3bda24c0bf23c7af980
RubyRed01 <104714810+RubyRed01@users.noreply.github.com>
2022-08-15 23:51:49 +1000

    extra polishing + demo video

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -23,7 +23,7 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{ model: Product, through: ProductTag, as: 'products_with_tag' }]
+      include: [{ model: Product, through: ProductTag, as: 'tags' }]
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -34,53 +34,40 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new tag
   try {
-    const tagData = await Tag.create({
-      tag_name: req.body.tag_name
-    });
+    const tagData = await Tag.create(req.body);
     res.status(200).json(tagData);
-  } catch (err) {
-    res.status(400).json(err);
+  } catch (error) {
+    res.status(400).json(error);
   }
 });
 
 router.put('/:id', async  (req, res) => {
   // update a tag's name by its `id` value
   try {
-    const tagData = await Tag.update(
-      {
-        tag_name: req.body.tag_name
-      },
-      {
-        where: {
-          id: req.params.id
-        }
-      }
-    );
-    if (!tagData) {
-      res.status(404).json({ message: 'There is no tag with that id.' });
+    const tagData = await Tag.update(req.body, {
+      where: { id: req.params.id },
+    });
+    if (!tagData[0]) {
+      res.status(404).json({ message: `There is no tag with this id.` });
       return;
     }
     res.status(200).json(tagData);
-  } catch (err) {
-    res.status(500).json(err);
+  } catch (error) {
+    res.status(400).json(error);
   }
 });
 
 router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
   try {
-    const tagData = await Tag.destroy({
-      where: {
-        id: req.params.id,
-      },
-    });
+    const tagData = await Tag.destroy({ where: { id: req.params.id } });
     if (!tagData) {
-      res.status(404).json({ message: 'There is no tag with that id.' });
+      res.status(404).json(`There is no tag with that id.`);
       return;
     }
     res.status(200).json(tagData);
-  } catch (err) {
-    res.status(500).json(err);
+  } catch (error) {
+    res.status(500).json(error);
   }
 });
 
